6c2392384c4f18da3429eb24174fa4bc3c07daeb
Shawn Shifflett <shawnshifflett@users.noreply.github.com>
2017-10-08 22:58:59 -0400

    Updated to add a quick help flag for some reason.

--- kblight.sh
+++ kblight.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# Quick script to pass a variable to wsconctl.
+# Created originally by Shawn Shifflett for use in OpenBSD
+# on 2011 MacBook Pro.
 
-echo "Setting keyboard to $1%"
+if [[ $1 == "-h" ]]; then
+        echo "Use 'kblight.sh [1-100]."
+else
+
 wsconsctl keyboard.backlight=$1%
+
+fi
